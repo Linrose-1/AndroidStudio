a4f1946150383b2fcf4299632443124838e079ad
Linrose-1 <3036359770@qq.com>
2024-05-15 16:51:30 +0800

    编写了联系人信息的类

---
+++ androidDemo/Contacts/app/src/main/java/com/rose/contacts/ContactInfo.java
@@ -0,0 +1,22 @@
+package com.rose.contacts;
+
+public class ContactInfo {
+    private String contactName;
+    private String phoneNumber;
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
