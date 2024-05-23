e6dbe2feeaaf089e512ebe88b353d63dc186ed19
Linrose-1 <3036359770@qq.com>
2024-05-23 11:42:54 +0800

    实现店铺列表界面显示功能-创建constant类实现从tomcat请求数据

--- androidDemo/order/app/build.gradle.kts
+++ androidDemo/order/app/build.gradle.kts
@@ -37,6 +37,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    //okhttp库
+    implementation("com.squareup.okhttp3:okhttp:3.12.0")
+    // Gson
+    implementation ("com.google.code.gson:gson:2.8.5")
 
 }
---
+++ androidDemo/order/app/src/main/java/com/rose/order/utils/Constant.java
@@ -0,0 +1,6 @@
+package com.rose.order.utils;
+
+public class Constant {
+    public static final String WEB_SITE = "localhost:80801/order";
+    public static final String REQUEST_SHOP_URL = "/shop_list_data.json";
+}
